b2b10e8eb3d74bfa6b156ca4ebb4ce6ce920d262
Kaminishikawahara KK <kkaori146@gmail.com>
2022-01-09 16:57:26 +0900

    Game: Color and emoji

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
+import emoji
+import colorama
+from colorama import Fore, Back, Style
 from random import randint
-print('=' * 20)
-print('Even or Odd Game')
-print('=' * 20)
+colorama.init(autoreset=True)
+print(f'{Fore.BLUE}=' * 20)
+print(f'{Fore.RED}Even or Odd Game')
+print(f'{Fore.BLUE}=' * 20)
 cont = 0
 while True:
     game = randint(0, 10)
-    user = int(input('Enter a value: '))
-    parimpar = str(input('Even or Odd [E/O]: ')).strip().upper()
+    user = int(input(f'{Fore.LIGHTMAGENTA_EX + Style.BRIGHT}Enter a value: '))
+    parimpar = str(input(f'{Fore.LIGHTYELLOW_EX + Style.BRIGHT}Even or Odd [E/O]: ')).strip().upper()
+    print(f'{Fore.BLUE}=' * 20)
     soma = game + user
     if soma % 2 == 0 and parimpar == 'E':
         cont += 1
@@ -14,7 +19,7 @@ while True:
         cont += 1
     else:
         break
-print('=-' * 25)
-print(f'You chose {user} and the game {game}. The total is {soma}!')
-print(f'\nGAME OVER!You won {cont} time(s)')
-print('=-' * 25)
+print(f'{Fore.CYAN}=-' * 25)
+print(f'{Back.LIGHTGREEN_EX + Fore.BLACK + Style.BRIGHT}You chose {user} and the game {game}. The total is {soma}!')
+print(emoji.emojize(f'{Back.LIGHTGREEN_EX + Fore.BLACK + Style.BRIGHT}GAME OVER!You won {cont} time(s) {Back.RESET +  Fore.RED} :fire: :fire: :fire: :fire: :fire: :fire:'))
+print(f'{Fore.CYAN}=-' * 25)
